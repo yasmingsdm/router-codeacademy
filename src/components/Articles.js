eb3bb6c6fbed49435d89bd43897596d493d55ed3
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-03-29 16:36:13 +0200

    finish

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -3,17 +3,17 @@ import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
 
-import {Link} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
+
 
 export default function Articles () {
   const articles = useSelector(selectArticles)
   
   // grab the search value from useLocation()
-  
+  const {search} = useLocation()
   // get the queryParams from new URLSearchParams() 
-  
-  const title = '' // <-- fix me!
-  
+  const queryParams = new URLSearchParams(search);
+  const title = queryParams.get('title')
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
   return (
